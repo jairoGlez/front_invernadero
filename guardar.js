f3e420d231b7ba3db1105070adf39da3ea40d948
Usuario <Usuario>
2017-06-28 11:28:22 -0500

    codigo arreglado

--- guardar.js
+++ guardar.js
@@ -5,11 +5,14 @@ $(document).ready(function(){
             url:"http://127.0.0.1:5000/agregar_usuario",
             data: $("#usuarios").serialize(),
             type: "POST",
-            succes: function(respuesta){
-                console.log("Guardado")
+            success: function(respuesta){
+                console.log(respuesta)
+                window.alert("Guardado!")
+                document.getElementById("usuarios").reset()
             },
             error: function(respuesta){
                 console.log(respuesta)
+                window.alert("Error")
             }
         })
     })  
